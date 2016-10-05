f55f691aec140126d3dc2ab1731be0de1d1b9c18
Jorge Gil <jorgegilcavazos@gmail.com>
2016-10-05 13:29:25 -0500

    Build project

---
+++ Game/android/src/com/goddard/nonamegame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.goddard.nonamegame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.goddard.nonamegame.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ Game/desktop/src/com/goddard/nonamegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.goddard.nonamegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.goddard.nonamegame.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
