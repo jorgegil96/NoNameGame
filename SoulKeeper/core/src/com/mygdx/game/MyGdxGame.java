15921791c8f9da2a1148be13221b6eb6a6a27f8d
Juan Garza <Juan Garza>
2016-10-08 17:04:46 -0500

    Subi SoulKeeper

---
+++ SoulKeeper/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,41 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screens.PlayScreen;
+
+public class MyGdxGame extends Game {
+    
+	public SpriteBatch batch;
+	public static final int V_WIDTH = 400;
+        public static final int V_HEIGHT = 208;
+	public static final float PPM = 100;
+
+	public static final short DEFAULT_BIT = 1;
+	public static final short SOULKEEPER_BIT = 2;
+	public static final short BRICK_BIT = 4;
+	public static final short COIN_BIT = 8;
+	public static final short DESTROYED_BIT = 16;
+	public static final short OBJECT_BIT = 32;
+	public static final short ENEMY_BIT = 64;
+	public static final short ITEM_BIT = 128;
+        
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		
+	}
+}
