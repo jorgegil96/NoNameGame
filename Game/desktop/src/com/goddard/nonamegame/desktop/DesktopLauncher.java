70862bc43ad551d544b198cada89fbd258afd533
caortiz97 <caortiz97@gmail.com>
2016-10-08 15:16:54 -0500

    Fullscreen added

--- Game/desktop/src/com/goddard/nonamegame/desktop/DesktopLauncher.java
+++ Game/desktop/src/com/goddard/nonamegame/desktop/DesktopLauncher.java
@@ -1,12 +1,18 @@
 package com.goddard.nonamegame.desktop;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.goddard.nonamegame.MainGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Graphics.DisplayMode[] modes = LwjglApplicationConfiguration.getDisplayModes();
+		Graphics.DisplayMode desktopMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
+		Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+config.setFromDisplayMode(displayMode);
 		new LwjglApplication(new MainGame(), config);
 	}
 }
