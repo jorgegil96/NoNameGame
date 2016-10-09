3e06c00be73e9e9474601bb21bd23cec1127c2f2
caortiz97 <caortiz97@gmail.com>
2016-10-09 06:57:41 -0500

    Fix p3

--- SoulKeeper/core/src/com/mygdx/game/Scenes/Hud.java
+++ SoulKeeper/core/src/com/mygdx/game/Scenes/Hud.java
@@ -11,11 +11,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-<<<<<<< HEAD
-import com.badlogic.gdx.math.Rectangle;
-=======
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
->>>>>>> 45d2238831550f3b59519dbc3b55c049ab007e04
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -23,14 +19,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-<<<<<<< HEAD
-import static com.mygdx.game.MyGdxGame.V_HEIGHT;
-import static com.mygdx.game.MyGdxGame.V_WIDTH;
-import org.w3c.dom.css.Rect;
-=======
 import com.mygdx.game.MyGdxGame;
 
->>>>>>> 45d2238831550f3b59519dbc3b55c049ab007e04
 
 
 /**
@@ -43,45 +33,6 @@ public class Hud implements Disposable{
     private MyGdxGame game;
     public Stage stage;
     private Viewport view;
-<<<<<<< HEAD
-    private Integer timer;
-    private float count;
-    private Integer score;
-    
-    Label countLabel;
-    Label scoreLabel;
-    Label lifeLabel;
-    Label levelLabel;
-    Label worldLabel;
-    Label marioLabel;
-    Label soulsLabel;
-    
-    public Hud(SpriteBatch sb){
-        timer = 300;
-        count = 0;
-        score = 0;
-        view = new FitViewport(V_WIDTH, V_HEIGHT, new OrthographicCamera());
-        stage = new Stage(view, sb);
-        
-        Table table = new Table();
-        table.top();
-        table.setFillParent(true);
-        
-        countLabel = new Label(String.format("%03d", timer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        lifeLabel = new Label("Life", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        soulsLabel = new Label("Souls", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        table.add(marioLabel).expandX().padTop(2);
-        table.add(lifeLabel).expandX().padTop(2);
-        table.add(soulsLabel).expandX().padTop(2);
-        table.row();
-        table.add(countLabel).expandX();
-        table.add().expandX();
-        table.add(countLabel).expandX();
-        stage.addActor(table);
-=======
     private Texture alma;
     private Texture notAlma;
     private Texture lifeBg;
@@ -120,7 +71,6 @@ public class Hud implements Disposable{
         lifeImg.setPosition(game.width*17/20+20,game.height*9/10+20);
         lifeImg.setScaleX(game.vida);
         stage.addActor(lifeImg);
->>>>>>> 45d2238831550f3b59519dbc3b55c049ab007e04
     }
 
     @Override
